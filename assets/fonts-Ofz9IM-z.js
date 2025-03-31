3a531c98813cf6a9a65c23ef7b0dcc69a876069d
tufcoder <tufcoder@users.noreply.github.com>
2025-03-31 21:44:38 +0000

    deploy: f58a69bb07998c9559b0735d63e8879f6cc762f1

---
+++ assets/fonts-Ofz9IM-z.js
@@ -0,0 +1,4 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-3lSaTjIr.js";import{M as e}from"./index-8Jemki5v.js";import{b as r}from"./index-FQKkaq7x.js";import{T as m}from"./TokensGrid-D8RQfoXz.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-D4MF2uSc.js";import"./index-C5IbEVfT.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function i(o){const n={h1:"h1",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{title:"Ignite-UI/Tokens/Font Families"}),`
+`,t.jsx(n.h1,{id:"font-families",children:"Font Families"}),`
+`,t.jsx(n.p,{children:"Essas são as cores utilizadas no Ignite UI."}),`
+`,t.jsx(m,{tokens:r})]})}function g(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{g as default};
