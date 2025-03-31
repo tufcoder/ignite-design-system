20e5215ef0b692d23200b2725c30834897a7e33b
tufcoder <tufcoder@users.noreply.github.com>
2025-03-31 20:01:07 +0000

    deploy: 605526d98787075be805d5bf67b907e4c1c3341d

---
+++ assets/TextInput.stories-Bb1LLjF2.js
@@ -0,0 +1,13 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{b as x,B as g,T as f}from"./index-ZCwoie_T.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-sY8i7rw0.js";import"./index-B2KKL2uI.js";const P={title:"Ignite-UI/Form/Text Input",component:x,tags:["autodocs"],args:{},decorators:[u=>a.jsxs(g,{as:"label",css:{display:"flex",flexDirection:"column",gap:"$2"},children:[a.jsx(f,{size:"sm",children:"Email address"}),u()]})]},r={args:{placeholder:"Type your email"}},e={args:{prefix:"cal.com/"}},s={args:{disabled:!0}};var o,t,c;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Type your email'
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var i,m,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    prefix: 'cal.com/'
+  }
+}`,...(n=(m=e.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var p,d,l;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(l=(d=s.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const D=["Primary","WithPrefix","Disabled"];export{s as Disabled,r as Primary,e as WithPrefix,D as __namedExportsOrder,P as default};
---
+++ assets/preview-CtMNPbLV.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-C5IbEVfT.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";const t={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},backgrounds:{default:"dark"},docs:{theme:r.dark}}};export{t as default};
