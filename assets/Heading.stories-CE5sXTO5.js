a26ab614970e5392249dc3a4672d9a6e96814345
tufcoder <tufcoder@users.noreply.github.com>
2025-04-01 21:21:12 +0000

    deploy: 1d32d5dcaa5872ab9ff37ff9ab9e6497a6cbfbda

---
+++ assets/Heading.stories-CE5sXTO5.js
@@ -0,0 +1,13 @@
+import{H as m}from"./index-BDypjoWS.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DBlixYoz.js";import"./index-B2KKL2uI.js";const h={title:"Ignite-UI/Typograph/Heading",component:m,tags:["autodocs"],args:{children:"Custom title",size:"md"},argTypes:{size:{options:["sm","md","lg","2xl","3xl","4xl","5xl","6xl"],control:{type:"inline-radio"}}}},r={},e={args:{children:"H1 Heading",as:"h1"},parameters:{docs:{description:{story:"Por padrão o heading sempre será um `h2`, mas podemos alterar isso com a propriedade `as`."}}}};var s,o,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var t,i,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'H1 Heading',
+    as: 'h1'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Por padrão o heading sempre será um \`h2\`, mas podemos alterar isso com a propriedade \`as\`.'
+      }
+    }
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const H=["Primary","CustomTag"];export{e as CustomTag,r as Primary,H as __namedExportsOrder,h as default};
---
+++ assets/Tooltip.stories-BmNZTcNM.js
@@ -0,0 +1,12 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{g as s,h as m,e as c}from"./index-BDypjoWS.js";import"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-DBlixYoz.js";import"./index-B2KKL2uI.js";const T={title:"Ignite-UI/Form/Tooltip",component:s,tags:["autodocs"],args:{date:new Date,available:!0},decorators:[e=>a.jsx(m,{children:a.jsx(e,{})})]},o={render:e=>a.jsx(s,{...e,children:a.jsx(c,{children:e.available?"Disponível":"Indisponível"})})},r={args:{available:!1},render:e=>a.jsx(s,{...e,children:a.jsx(c,{children:e.available?"Disponível":"Indisponível"})})};var t,n,l;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => <Tooltip {...args}>
+      <Button>{args.available ? 'Disponível' : 'Indisponível'}</Button>
+    </Tooltip>
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var i,p,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    available: false
+  },
+  render: args => <Tooltip {...args}>
+      <Button>{args.available ? 'Disponível' : 'Indisponível'}</Button>
+    </Tooltip>
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const h=["Available","Unavailable"];export{o as Available,r as Unavailable,h as __namedExportsOrder,T as default};
