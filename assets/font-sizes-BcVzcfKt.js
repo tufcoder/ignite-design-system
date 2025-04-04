0b136f870e7acc5b2c613c3849caaeed74f4f04c
tufcoder <tufcoder@users.noreply.github.com>
2025-04-04 16:17:25 +0000

    deploy: 4bcdd2248caf5bcc7497aa51ded3d0df2f0fc262

---
+++ assets/font-sizes-BcVzcfKt.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-BDhFyJyw.js";import{useMDXComponents as s}from"./index-3lSaTjIr.js";import{M as r}from"./index-CyKgvtAz.js";import{f as i}from"./index-C8vskQuK.js";import{T as m}from"./TokensGrid-BhYZdsn8.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./index-CgfFrydU.js";import"./iframe-DKyRQXIJ.js";import"./index-C5IbEVfT.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function e(o){const n={h1:"h1",p:"p",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Ignite-UI/Tokens/Font Sizes"}),`
+`,t.jsx(n.h1,{id:"font-sizes",children:"Font Sizes"}),`
+`,t.jsx(n.p,{children:"Essas são as cores utilizadas no Ignite UI."}),`
+`,t.jsx(m,{tokens:i,hasRemValue:!0})]})}function I(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{I as default};
---
+++ assets/line-heights-Ddok1OL2.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-BDhFyJyw.js";import{useMDXComponents as i}from"./index-3lSaTjIr.js";import{M as s}from"./index-CyKgvtAz.js";import{l as r}from"./index-C8vskQuK.js";import{T as m}from"./TokensGrid-BhYZdsn8.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-B2KKL2uI.js";import"./index-CgfFrydU.js";import"./iframe-DKyRQXIJ.js";import"./index-C5IbEVfT.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",p:"p",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Ignite-UI/Tokens/Line Heights"}),`
+`,t.jsx(o.h1,{id:"line-heights",children:"Line Heights"}),`
+`,t.jsx(o.p,{children:"Essas são as cores utilizadas no Ignite UI."}),`
+`,t.jsx(m,{tokens:r})]})}function I(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{I as default};
